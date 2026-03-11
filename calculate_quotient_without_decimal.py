b55aea0bfa37bc615e8d00e0709f441459437592
Duke Buerano <dukebueranoo@gmail.com>
2026-03-11 10:39:15 +0800

    final commit

--- calculate_quotient_without_decimal.py
+++ calculate_quotient_without_decimal.py
@@ -21,3 +21,7 @@ def calculate_floor_quotient():
     else:
         # Print an error message in red text so the user knows what went wrong
         print("\033[91mCannot divide by zero.\033[0m")
+# This part tells the computer to actually start the program
+if __name__ == "__main__":
+    # Run the floor quotient command we made at the top
+    calculate_floor_quotient()
