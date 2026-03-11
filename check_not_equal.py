9a2cd967e7a8353f1d025313e733ec7e4b886007
Duke Buerano <dukebueranoo@gmail.com>
2026-03-11 10:20:00 +0800

    final commit

--- check_not_equal.py
+++ check_not_equal.py
@@ -12,3 +12,7 @@ def check_if_not_equal():
     if first_number != second_number:
         # If they are different, print "Not Equal" in green text
         print("\033[92mNot Equal\033[0m")
+# This part tells the computer to actually start the program
+if __name__ == "__main__":
+    # Run the command we made at the top
+    check_if_not_equal()
